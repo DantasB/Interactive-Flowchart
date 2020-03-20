89d0aeebc6534cbb12d1c8f1cdd671336501553a
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-03-20 02:59:41 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -570,23 +570,6 @@ function handleOptionsMenu(){
 		// Saves the cookie:
 		setCookie("colorId", colorId.toString());
 	});
-
-	//////////////////////////
-	/// PERIOD CHANGE ICON ///
-	//////////////////////////
-	
-	// Switch option icon:
-	var moonButton = $("#moon");	
-	moonButton.on(mouseDown, function(){
-    		var cookies = document.cookie.split(";");
-
-    		for (var i = 0; i < cookies.length; i++) {
-        		var cookie = cookies[i];
-        		var eqPos = cookie.indexOf("=");
-        		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    		}
-	});
 	
 	//////////////////////////
 	/// SWITCH OPTION ICON ///
