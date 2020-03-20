ec2d7599f2fa6d5b96bcb7965c7c2a4d21bb52b1
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-03-20 03:00:11 -0300

    Update indexNight.js

--- js/indexNight.js
+++ js/indexNight.js
@@ -571,24 +571,6 @@ function handleOptionsMenu(){
 		setCookie("colorId", colorId.toString());
 	});
 
-
-	//////////////////////////
-	/// PERIOD CHANGE ICON ///
-	//////////////////////////
-	
-	// Switch option icon:
-	var sunButton = $("#sun");	
-	sunButton.on(mouseDown, function(){
-    		var cookies = document.cookie.split(";");
-
-    		for (var i = 0; i < cookies.length; i++) {
-        		var cookie = cookies[i];
-        		var eqPos = cookie.indexOf("=");
-        		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    		}
-	});
-	
 	//////////////////////////
 	/// SWITCH OPTION ICON ///
 	//////////////////////////
