1a28f46d1cd32ce163fc7d865fae8a7ed0bdd5f1
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2021-03-14 01:56:27 -0300

    Update database_eq.js

--- js/database_eq.js
+++ js/database_eq.js
@@ -59,133 +59,133 @@ window.courses = [{
         credits: 4,
         code: "IQA121",
         semester: 2,
-        requirements: [3]
+        requirements: [4]
     },
     {
         name: "Química Analítica Exp. I",
         credits: 2,
         code: "IQO128",
         semester: 2,
-        requirements: [2]
+        requirements: [3]
     },
     {
         name: "Química Organica I",
         credits: 4,
         code: "IQO128",
         semester: 2,
-        requirements: [4]
+        requirements: [5]
     },
     {
         name: "Química Orgânica Exp. I - EQ",
         credits: 2,
         code: "IQO130",
         semester: 2,
-        requirements: [2]
+        requirements: [3]
     },
     {
         name: "Cálculo II",
         credits: 4,
         code: "MAC128",
         semester: 2,
-        requirements: [4]
+        requirements: [5]
     },
     {
         name: "Fisica III",
         credits: 4,
         code: "FIM230",
         semester: 3,
-        requirements: [11, 6]
+        requirements: [12, 7]
     },
     {
         name: "Física Experimental III",
         credits: 1,
         code: "FIN231",
         semester: 3,
-        requirements: [1]
+        requirements: [2]
     },
     {
         name: "Química Analítica Exp II",
         credits: 1,
         code: "IQA243",
         semester: 3,
-        requirements: [7, 8]
+        requirements: [8, 9]
     },
     {
         name: "Termodinâmica Clássica",
         credits: 4,
         code: "IQA123",
         semester: 3,
-        requirements: [11]
+        requirements: [12]
     },
     {
         name: "Química Experimental",
         credits: 2,
         code: "IQG231",
         semester: 3,
-        requirements: [3]
+        requirements: [4]
     },
     {
         name: "Química Orgânica II",
         credits: 4,
         code: "IQO235",
         semester: 3,
-        requirements: [9]
+        requirements: [10]
     },
     {
         name: "Cálculo III",
         credits: 4,
         code: "MAC238",
         semester: 3,
-        requirements: [11]
+        requirements: [12]
     },
     {
         name: "Bioquímica EQ",
         credits: 5,
         code: "IQB248",
         semester: 4,
-        requirements: [15, 17]
+        requirements: [16, 18]
     },
     {
         name: "Fenômeno de Superfície e Eletroq",
         credits: 4,
         code: "IQF351",
         semester: 4,
-        requirements: [15]
+        requirements: [16]
     },
     {
         name: "Equi Fases Sist Multicompostos",
         credits: 2,
         code: "IQF353",
         semester: 4,
-        requirements: [15]
+        requirements: [16]
     },
     {
         name: "Química Orgânica Exp Ii-eq",
         credits: 2,
         code: "IQO246",
         semester: 4,
-        requirements: [9, 10]
+        requirements: [10, 11]
     },
     {
         name: "Calculo IV",
         credits: 4,
         code: "MAC248",
         semester: 4,
-        requirements: [11]
+        requirements: [12]
     },
     {
         name: "Álgebra Linear II - EQ",
         credits: 4,
         code: "MAE245",
         semester: 4,
-        requirements: [11]
+        requirements: [12]
     },
     {
         name: "Microbiologia Industrial",
         credits: 3,
         code: "EQB353",
         semester: 5,
-        requirements: [19]
+        requirements: [20]
     },
     {
         name: "Mecânica dos Fluídos",
@@ -210,35 +210,35 @@ window.courses = [{
         credits: 4,
         code: "IQF361",
         semester: 5,
-        requirements: [7, 8]
+        requirements: [8, 9]
     },
     {
         name: "Físico-química Experimental/eq",
         credits: 1,
         code: "IQF367",
         semester: 5,
-        requirements: [19]
+        requirements: [20]
     },
     {
         name: "Estatística",
         credits: 4,
         code: "MAD469",
         semester: 5,
-        requirements: [19]
+        requirements: [20]
     },
     {
         name: "Fundam de Engen Bioquimica II",
         credits: 3,
         code: "EQB363",
         semester: 6,
-        requirements: [31]
+        requirements: [32]
     },
     {
         name: "Ciências Soc e Introd à Econom",
         credits: 3,
         code: "EQE360",
         semester: 6,
-        requirements: [31]
+        requirements: [32]
     },
     {
         name: "Cinética Química e Reatores",
@@ -269,21 +269,21 @@ window.courses = [{
         credits: 4,
         code: "IQG244",
         semester: 6,
-        requirements: [3]
+        requirements: [4]
     },
     {
         name: "Modelagem e Dinâmica de Proces",
         credits: 3,
         code: "EQE400",
         semester: 7,
-        requirements: [32]
+        requirements: [33]
     },
     {
         name: "Admin.e Organização Industrial",
         credits: 3,
         code: "EQE470",
         semester: 7,
-        requirements: [33]
+        requirements: [34]
     },
     {
         name: "Operações Unitárias I",
@@ -308,28 +308,28 @@ window.courses = [{
         credits: 2,
         code: "EQI472",
         semester: 7,
-        requirements: [31]
+        requirements: [32]
     },
     {
         name: "Processos Orgânicos",
         credits: 4,
         code: "EQO475",
         semester: 7,
-        requirements: [11]
+        requirements: [12]
     },
     {
         name: "Engenharia do Meio Ambiente",
         credits: 4,
         code: "EQB485",
         semester: 8,
-        requirements: [39]
+        requirements: [20]
     },
     {
         name: "Operações Unitárias II",
         credits: 5,
         code: "EQB482",
         semester: 8,
-        requirements: [33]
+        requirements: [34]
     },
     {
         name: "Planej e Avaliação de Projetos",
@@ -360,7 +360,7 @@ window.courses = [{
         credits: 3,
         code: "EQB592",
         semester: 9,
-        requirements: [31]
+        requirements: [32]
     },
     {
         name: "Tecnologia Orgânica experimental",
